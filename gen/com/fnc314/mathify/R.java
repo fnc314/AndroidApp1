cdb63e2f27d374c2d4779cc8afb219b8b579aabc
Franco <fnc314@gmail.com>
2014-03-09 23:28:22 -0700

    entry page laid out

--- gen/com/fnc314/mathify/R.java
+++ gen/com/fnc314/mathify/R.java
@@ -995,15 +995,17 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050047;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
+        public static final int diff=0x7f050041;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
@@ -1018,11 +1020,18 @@ containing a value of this type.
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
+        public static final int mod1=0x7f050045;
+        public static final int mod2=0x7f050046;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int num1=0x7f05003d;
+        public static final int num2=0x7f05003e;
+        public static final int prod=0x7f050042;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
+        public static final int quot1=0x7f050043;
+        public static final int quot2=0x7f050044;
         public static final int radio=0x7f05002f;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
@@ -1040,6 +1049,7 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
+        public static final int sum=0x7f050040;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
@@ -1146,9 +1156,17 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int diff=0x7f0a0014;
         public static final int hello_world=0x7f0a000e;
+        public static final int mathify=0x7f0a0012;
+        public static final int mod1=0x7f0a0018;
+        public static final int mod2=0x7f0a0019;
         public static final int num1=0x7f0a0010;
         public static final int num2=0x7f0a0011;
+        public static final int prod=0x7f0a0015;
+        public static final int quot1=0x7f0a0016;
+        public static final int quot2=0x7f0a0017;
+        public static final int sum=0x7f0a0013;
     }
     public static final class style {
         /** 
